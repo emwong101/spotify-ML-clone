cecd2896364a4fc0433bc17704fce3517923cdef
Allan Somera <nagal.somera@gmail.com>
2023-01-06 21:18:21 -0800

    moved nodemon to devDependencies, created files for basic BE

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require("node:path");
+
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+
+const express = require("express");
+const server = express();
+const cors = require("cors");
+
+server.use(express.json());
+server.use(cors());
+
+const PORT = process.env.PORT || 5500;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT} `);
+});
