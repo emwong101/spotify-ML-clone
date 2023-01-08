033c458c6e3c895c9f04c13e395e286fdca15c6e
emwong101 <emilywonglfa@gmail.com>
2023-01-07 16:04:04 -0800

    fixed env file process

--- client/src/Pages/Discover/Discover.jsx
+++ client/src/Pages/Discover/Discover.jsx
@@ -2,27 +2,29 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Discover() {
-  const BASE_URL = process.env.REACT_APP_BASE_URL;
-  const TOKEN = process.env.TOKEN;
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const TOKEN = import.meta.env.VITE_TOKEN;
 
-  const searchSpotify = () => {
-    // try {
-    //   const searchQuery = "anderson paak";
-    //   const typeQuery = `type=artist`;
-    //   const { data } = await axios.get(
-    //     `${BASE_URL}/search?q=${searchQuery}&${typeQuery}`,
-    //     {
-    //       headers: {
-    //         Authorization: `Bearer ${TOKEN}`,
-    //       },
-    //     }
-    //   );
-    //   console.log(data);
-    //   console.log("search spotify clicked");
-    // } catch {
-    //   console.log("Error");
-    // }
-    console.log(BASE_URL);
+  const authConfig = `Bearer ${TOKEN}`;
+  console.log(authConfig);
+
+  const searchSpotify = async (e) => {
+    try {
+      const searchQuery = "anderson paak";
+      const typeQuery = `type=artist`;
+      const { data } = await axios.get(
+        `${BASE_URL}search?q=${searchQuery}&${typeQuery}`,
+        {
+          headers: {
+            Authorization: authConfig,
+          },
+        }
+      );
+      console.log(data);
+      console.log("search spotify clicked");
+    } catch {
+      console.log("Error");
+    }
   };
 
   return (
@@ -30,14 +32,14 @@ function Discover() {
       <h1>Discover Playlists</h1>
       <section className="discover__form">
         <h2>Form</h2>
-        <form action="">
-          <label htmlFor="mood">
-            <h3>Mood</h3>
-            insert moods here
-          </label>
-          <label htmlFor="timeframe">insert timeframes here</label>
-          <button onClick={searchSpotify}>Discover</button>
-        </form>
+        {/* <form action=""> */}
+        <label htmlFor="mood">
+          <h3>Mood</h3>
+          insert moods here
+        </label>
+        <label htmlFor="timeframe">insert timeframes here</label>
+        <button onClick={searchSpotify}>Discover</button>
+        {/* </form> */}
       </section>
       <section className="discover__results">
         <h2>Results</h2>
