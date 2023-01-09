4288276ef565c0ee524c9df32763aacea52ae60c
Allan Somera <nagal.somera@gmail.com>
2023-01-09 10:51:44 -0800

    adjusted seed, added landing page

---
+++ client/src/Pages/Landing/Landing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+//
+function Login() {
+  // const { data } = await axios.get(
+  //   "http://localhost:8080/user/1/getspotifytoken"
+  // );
+
+  return (
+    <>
+      <div>Landing</div>
+    </>
+  );
+}
+
+export default Login;
