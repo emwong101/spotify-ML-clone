d2e1eaab67ab676152e9ca8d75f930072af6623e
Allan Somera <nagal.somera@gmail.com>
2023-01-11 03:14:25 -0800

    added some color to the landing page

--- client/src/Context/UserContext.jsx
+++ client/src/Context/UserContext.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const UserContext = createContext({
   profile: {},
@@ -11,57 +11,13 @@ export const UserContext = createContext({
 const UserProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState({});
 
-  // const grabProfile = async () => {
-  //   //axios call here
-  //   const response = await axios.get('http://localhost:8080/auth/profile', {
-  //     withCredentials: true,
-  //   });
-  //   // .then((res) => {
-  //   //   // console.log("landing auth: ", res);
-
-  //   //   // if (res.status === 200) {
-  //   //   //   let navigate = useNavigate();
-  //   //   //   navigate("/charts");
-  //   //   //   return;
-  //   //   setIsLoggedIn({ data: res.data, auth: true });
-  //   //   console.log(res);
-  //   //   console.log('function is ran');
-  //   // })
-  //   // .catch((err) => {
-  //   //   if (err.response.status === 401) {
-  //   //     // Update the state: done authenticating, user is not logged in
-  //   //   } else {
-  //   //     console.log('Error authenticating', err);
-  //   //   }
-  //   // });
-  //   setIsLoggedIn({ data: response.data, auth: true });
-  //   console.log(isLoggedIn);
-  // };
-
-  // const login = () => {
-  //   const response = axios
-  //     .get('http://localhost:8080/auth/spotify', {
-  //       withCredentials: true,
-  //     })
-  //     .then((res) => {
-  //       setIsLoggedIn((user) => ({ data: res.data, auth: true }));
-  //     })
-  //     .catch((err) => {
-  //       if (err.response.status === 401) {
-  //         // Update the state: done authenticating, user is not logged in
-  //       } else {
-  //         console.log('Error Logging in', err);
-  //       }
-  //     });
-  // };
-
   const setProfileData = (profileData) => {
     setIsLoggedIn({ data: profileData, auth: true });
   };
 
   const logout = () => {
     const response = axios
-      .get('http://localhost:8080/auth/logout', {
+      .get("http://localhost:8080/auth/logout", {
         withCredentials: true,
       })
       .then((res) => {
@@ -71,7 +27,7 @@ const UserProvider = ({ children }) => {
         if (err.response.status === 401) {
           // Update the state: done authenticating, user is not logged in
         } else {
-          console.log('Error Logging out', err);
+          console.log("Error Logging out", err);
         }
       });
   };
