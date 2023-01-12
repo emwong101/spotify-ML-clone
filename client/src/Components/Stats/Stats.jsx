c414893272c2f5f674ad5f6fa9c9a62b034e580f
unknown <jackytam2020@gmail.com>
2023-01-11 21:50:49 -0800

    changed axios calls for top artists to happen on component mount

--- client/src/Components/Stats/Stats.jsx
+++ client/src/Components/Stats/Stats.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { UserContext } from '../../Context/UserContext';
 import axios from 'axios';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
@@ -11,6 +11,9 @@ import './Stats.scss';
 function Stats(props) {
   const user = useContext(UserContext);
   const [genreStats, setGenreStats] = useState({});
+  const [fourWeeksArtists, setFourWeeksArtists] = useState([]);
+  const [sixMonthsArtists, setSixMonthsArtists] = useState([]);
+  const [allTimeArtists, setAllTimeArtists] = useState([]);
 
   const getGenreStats = (artistList) => {
     //define an object
@@ -29,7 +32,7 @@ function Stats(props) {
       });
     });
 
-    return genres;
+    setGenreStats(genres);
   };
 
   const url = 'https://api.spotify.com/v1/me/top/artists';
@@ -49,7 +52,7 @@ function Stats(props) {
         }
       });
 
-    setGenreStats(getGenreStats(data.items));
+    setFourWeeksArtists(data.items);
   };
 
   const topArtistsSixMonths = async () => {
@@ -61,7 +64,7 @@ function Stats(props) {
       },
     });
 
-    setGenreStats(getGenreStats(data.items));
+    setSixMonthsArtists(data.items);
   };
 
   const topArtistsAllTime = async () => {
@@ -73,7 +76,7 @@ function Stats(props) {
       },
     });
 
-    setGenreStats(getGenreStats(data.items));
+    setAllTimeArtists(data.items);
   };
 
   const genrePieChart = (
@@ -97,12 +100,42 @@ function Stats(props) {
       }}
     />
   );
+
+  //grab all data on component mount
+  useEffect(() => {
+    topArtistsFourWeeks();
+    topArtistsSixMonths();
+    topArtistsAllTime();
+  }, []);
+
+  useEffect(() => {
+    //initial stats should show 4weeks top artists when component mounts
+    getGenreStats(fourWeeksArtists);
+  }, [fourWeeksArtists]);
   return (
     <div className="stats">
       <div>
-        <button onClick={topArtistsFourWeeks}>Last 4 Weeks</button>
-        <button onClick={topArtistsSixMonths}>6 Months</button>
-        <button onClick={topArtistsAllTime}>All time</button>
+        <button
+          onClick={() => {
+            getGenreStats(fourWeeksArtists);
+          }}
+        >
+          Last 4 Weeks
+        </button>
+        <button
+          onClick={() => {
+            getGenreStats(sixMonthsArtists);
+          }}
+        >
+          6 Months
+        </button>
+        <button
+          onClick={() => {
+            getGenreStats(allTimeArtists);
+          }}
+        >
+          All time
+        </button>
       </div>
       {genrePieChart}
     </div>
