d8ff1fd383e266292fcfedb9ce7953e176b26bca
unknown <jackytam2020@gmail.com>
2023-01-12 18:00:38 -0800

    added slider for playlist length

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState, useContext } from "react";
-import "./App.scss";
-import Header from "./Components/Header/Header";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import "vite/modulepreload-polyfill";
+import { useState, useContext } from 'react';
+import './App.scss';
+import Header from './Components/Header/Header';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import 'vite/modulepreload-polyfill';
 
-import UserProvider from "./Context/UserContext";
-import Discover from "./pages/Discover/Discover";
-import Charts from "./pages/Charts/Charts";
+import UserProvider from './Context/UserContext';
+import Discover from './pages/Discover/Discover';
+import Charts from './pages/Charts/Charts';
 
-import Landing from "./pages/Landing/Landing";
-import Profile from "./pages/Profile/Profile";
-import Mood from "./pages/Mood/Mood";
-import Playlistgen from "./pages/Playlistgen/Playlistgen";
+import Landing from './pages/Landing/Landing';
+import Profile from './pages/Profile/Profile';
+import Mood from './pages/Mood/Mood';
+import Playlistgen from './pages/Playlistgen/Playlistgen';
 
-import { UserContext } from "./Context/UserContext";
-import PlaylistLength from "./pages/playlistLength/PlaylistLength";
+import { UserContext } from './Context/UserContext';
+import PlaylistLength from './Pages/PlaylistLength/PlaylistLength';
 
 function App() {
   // const { isLoggedIn } = useContext(UserContext);
--- client/src/Pages/playlistLength/PlaylistLength.jsx
+++ client/src/Pages/playlistLength/PlaylistLength.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { useContext } from "react";
-import { UserContext } from "../../Context/UserContext";
-import "./PlaylistLength.scss";
-import axios from "axios";
+import React from 'react';
+import { useContext, useState } from 'react';
+import { UserContext } from '../../Context/UserContext';
+import './PlaylistLength.scss';
+import axios from 'axios';
+import Slider from '@mui/material/Slider';
 
 function PlaylistLength() {
+  const [length, setLength] = useState(5);
   const user = useContext(UserContext);
   const BASE_URL = import.meta.env.VITE_BASE_URL;
+  // const BASE_URL = 'https://api.spotify.com/v1/';
 
   const getRecommendations = async (e) => {
     try {
       const { data } = await axios.get(
-        `${BASE_URL}recommendations?limit=5&seed_artists=${user.artists.join(
-          ","
+        `${BASE_URL}recommendations?limit=${length}&seed_artists=${user.artists.join(
+          ','
         )}&${user.mood}`,
         {
           headers: { Authorization: `Bearer ${user.profile.access_token}` },
@@ -20,12 +23,30 @@ function PlaylistLength() {
       );
       console.log(data);
     } catch {
-      console.log("Error");
+      console.log('Error');
     }
     console.log(user.mood);
   };
+
+  console.log(user);
+
   return (
     <div>
+      <div className="playlist-length__slider">
+        <Slider
+          min={5}
+          max={100}
+          value={length}
+          onChange={(e) => {
+            setLength(e.target.value);
+          }}
+          defaultValue={5}
+          aria-label="Default"
+          valueLabelDisplay="auto"
+        />
+        <p>Playlist Length: {length}</p>
+      </div>
+
       <button onClick={getRecommendations}>DONE</button>
     </div>
   );
