5f2fd89d5402491db1458e0e8e2c08978786dea0
unknown <jackytam2020@gmail.com>
2023-01-16 14:55:09 -0800

    added grab new access token functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,44 @@
-import { useState, useContext } from "react";
-import "./App.scss";
-import Header from "./Components/Header/Header";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import "vite/modulepreload-polyfill";
+import { useState, useContext, useEffect } from 'react';
+import './App.scss';
+import Header from './Components/Header/Header';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import 'vite/modulepreload-polyfill';
 
-import UserProvider from "./Context/UserContext";
-import Discover from "./Pages/Discover/Discover";
-import Charts from "./Pages/Charts/Charts";
+import UserProvider from './Context/UserContext';
+import Discover from './Pages/Discover/Discover';
+import Charts from './Pages/Charts/Charts';
 
-import Landing from "./Pages/Landing/Landing";
-import Profile from "./Pages/Profile/Profile";
-import Mood from "./Pages/Mood/Mood";
-import Playlistgen from "./Pages/Playlistgen/Playlistgen";
+import Landing from './Pages/Landing/Landing';
+import Profile from './Pages/Profile/Profile';
+import Mood from './Pages/Mood/Mood';
+import Playlistgen from './Pages/Playlistgen/Playlistgen';
 
-import { UserContext } from "./Context/UserContext";
-import PlaylistLength from "./Pages/playlistLength/PlaylistLength";
+import PlaylistLength from './Pages/playlistLength/PlaylistLength';
+import axios from 'axios';
+import { UserContext } from './Context/UserContext';
 
 function App() {
-  // const { isLoggedIn } = useContext(UserContext);
+  const user = useContext(UserContext);
+  const userProfile = JSON.parse(localStorage.getItem('user profile'));
+
+  const grabNewAccessToken = () => {
+    const response = axios
+      .get('http://localhost:8080/refresh', { withCredentials: true })
+      .then((res) => {
+        console.log(res);
+        user.setProfileData(res.data);
+        localStorage.setItem('user profile', JSON.stringify(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      grabNewAccessToken();
+    }, userProfile.expiry * 1000);
+  }, []);
 
   return (
     <>
