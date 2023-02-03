6049e30e1133ab8caa22772eb7cc0a12cc926024
emwong101 <emilywonglfa@gmail.com>
2023-02-03 15:30:55 -0800

    added login button and began building landing page cards

--- client/src/Components/LandingGradient/LandingGradient.jsx
+++ client/src/Components/LandingGradient/LandingGradient.jsx
@@ -1,12 +1,12 @@
 import "./LandingGradient.scss";
 
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useRef } from "react";
+import { useEffect, useContext, useState } from "react";
+import { UserContext } from "../../Context/UserContext";
+import Mixtape from "/assets/mixtape-background.svg";
 
 function LandingGradient() {
-  const tracker = useRef();
+  const user = useContext(UserContext);
   const mouseTracker = () => {
     const [mousePosition, setMousePosition] = useState({ x: null, y: null });
 
@@ -32,13 +32,45 @@ function LandingGradient() {
     <div>
       <div
         className="main__container"
-        ref={tracker}
         style={{ "--x": positionX, "--y": positionY }}
       >
         <h1 className="tagline">
-          Build the mixtape <br />
-          of your dreams
+          Build the <span className="mixtape">mixtape</span> of <br />
+          your dreams
         </h1>
+        <div className="landing__card">
+          <img className="landing__card--banner" src={Mixtape} />
+          <div className="landing__card--text">
+            <h5 className="landing__card--title">Step 3: Get Your Mixtape!</h5>
+            <p className="landing__card--body">text goes here</p>
+          </div>
+        </div>
+        {user.profile.id ? (
+          <div className="landing__profile-container">
+            <div className="landing__profile">{user.profile.spotify_id}</div>
+            <div>
+              <a href="http://localhost:8080/auth/logout">
+                <button
+                  type="button"
+                  className="landing__button"
+                  onClick={() => {
+                    sessionStorage.clear();
+                  }}
+                >
+                  Logout
+                </button>
+              </a>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <a href="http://localhost:8080/auth/spotify">
+              <button type="button" className="landing__button">
+                Login
+              </button>
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
