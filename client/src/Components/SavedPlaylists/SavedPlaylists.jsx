76f41597d0db59165c891e179cba3b338c92546c
Allan Somera <nagal.somera@gmail.com>
2023-02-07 08:33:16 -0800

    added carousel for the cd images, TODO: make it so accordion will have a max width

--- client/src/Components/SavedPlaylists/SavedPlaylists.jsx
+++ client/src/Components/SavedPlaylists/SavedPlaylists.jsx
@@ -1,13 +1,13 @@
 import "./SavedPlaylists.scss";
 import axios from "axios";
+import React from "react";
+import Carousel from "nuka-carousel";
 import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../Context/UserContext";
 import prettyMilliseconds from "pretty-ms";
 
 const SavedPlaylists = ({ playlist_data }) => {
   let user = useContext(UserContext);
-  // console.log("playlist_data: ", JSON.parse(playlist_data));
-
   const renderPlaylists = () => {
     // const pl_data = user.savedplaylists;
     if (Object.keys(playlist_data).length !== 0) {
@@ -15,43 +15,45 @@ const SavedPlaylists = ({ playlist_data }) => {
         <>
           <div className="saved-pl">
             <div className="saved-pl__tracklist">
-              {JSON.parse(playlist_data).map((track) => (
-                <div className="saved-pl__track" key={`${track.id}`}>
-                  <div className="saved-pl__track-primary-con">
-                    <div className="saved-pl__track-cover-con">
-                      <div
-                        className="saved-pl__track-cover"
-                        style={{
-                          backgroundImage: `url(${track.album.images[2].url})`,
-                        }}
-                      >
-                        <div className="inner-circle-w"></div>
-                        <div className="inner-circle-g"></div>
+              <Carousel slidesToShow={5}>
+                {JSON.parse(playlist_data).map((track) => (
+                  <div className="saved-pl__track" key={`${track.id}`}>
+                    <div className="saved-pl__track-primary-con">
+                      <div className="saved-pl__track-cover-con">
+                        <div
+                          className="saved-pl__track-cover"
+                          style={{
+                            backgroundImage: `url(${track.album.images[2].url})`,
+                          }}
+                        >
+                          <div className="inner-circle-w"></div>
+                          <div className="inner-circle-g"></div>
+                        </div>
                       </div>
+                      {/* title-album
+                       * <div className="saved-pl__title-album-con">
+                       *   <div className="saved-pl__track-title">{track.name}</div>
+                       *   {#<{(|
+                       *    * <div className="saved-pl__track-album">
+                       *    *   {track.album.name}
+                       *    * </div>
+                       *    |)}>#}
+                       *   <div className="saved-pl__track-artist">
+                       *     {track.artists[0].name}
+                       *   </div>
+                       * </div>
+                       */}
                     </div>
-                    {/* title-album
-                     * <div className="saved-pl__title-album-con">
-                     *   <div className="saved-pl__track-title">{track.name}</div>
-                     *   {#<{(|
-                     *    * <div className="saved-pl__track-album">
-                     *    *   {track.album.name}
-                     *    * </div>
-                     *    |)}>#}
-                     *   <div className="saved-pl__track-artist">
-                     *     {track.artists[0].name}
-                     *   </div>
+                    {/* duration
+                     * <div className="saved-pl__track-duration">
+                     *   {prettyMilliseconds(track.duration_ms, {
+                     *     secondsDecimalDigits: 0,
+                     *   })}
                      * </div>
                      */}
                   </div>
-                  {/* duration
-                   * <div className="saved-pl__track-duration">
-                   *   {prettyMilliseconds(track.duration_ms, {
-                   *     secondsDecimalDigits: 0,
-                   *   })}
-                   * </div>
-                   */}
-                </div>
-              ))}
+                ))}
+              </Carousel>
             </div>
           </div>
         </>
